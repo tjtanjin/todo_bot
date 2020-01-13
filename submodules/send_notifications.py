81c0772ebfb85a926affa043a4514acc7d8e2c0c
tjtanjin <cjtanjin@gmail.com>
2020-01-14 05:16:31 +0800

    Added function to send notifications daily. Currently set to 8am. Will update to 12midnight soon.

---
+++ submodules/send_notifications.py
@@ -0,0 +1,48 @@
+import json, requests, datetime
+
+def check_user_tasks(context):
+	print("Checking to send reminders...")
+	with open("./config/endpoint.json", "r") as file:
+		endpoint = json.load(file)["endpoint"]
+	with open("./config/bot.json", "r") as file:
+		creds = json.load(file)
+	auth_res = requests.post(endpoint + "/authenticate", data = {"email": creds["email"], "password": creds["password"]})
+	token = json.loads(auth_res.content)["auth_token"]
+	get_user_res = requests.get(endpoint + "/users", headers = { "Authorization": "Bearer " + token})
+	users = json.loads(get_user_res.content)
+	for user in users:
+		if user["telegram_notifications"] == "1" and user["telegram_id"] != None:
+			get_task_res = requests.get(endpoint + "/users/" + str(user["id"]) + "/tasks", headers = { "Authorization": "Bearer " + token})
+			tasks = json.loads(get_task_res.content)
+			expiring_tasks = []
+			current_date = datetime.date.today()
+			for task in tasks:
+				days_left = getDifference(task["deadline"], current_date)
+				if days_left <= 3:
+					expiring_tasks.append([task["task_name"], days_left])
+			send_reminders(user["name"], user["telegram_id"], expiring_tasks, context)
+
+def getDifference(deadline, current_date): 
+	deadlineparts = deadline.split('-');
+	new_deadline = datetime.date(int(deadlineparts[0]), int(deadlineparts[1]), int(deadlineparts[2]))
+	return (new_deadline - current_date).days
+
+def send_reminders(todo_name, telegram_id, tasks, context):
+	string = "Hello " + todo_name + ", this is a daily reminder that your following tasks are expiring soon:\n\n" + "Task Name ----- Days Left\n"
+	for task in tasks:
+		string = string + task[0] + " ----- " + str(task[1]) + "\n"
+	context.bot.send_message(chat_id=telegram_id, text=string) 
+
+def callback_timer(update, context):
+	with open("./config/bot.json", "r") as file:
+		is_su = json.load(file)["su"]
+	if str(update.message.chat_id) == is_su:
+		print("Queue started")
+		context.job_queue.run_daily(check_user_tasks, datetime.time(8, 00, 00), context=context)
+
+def stop_timer(update, context):
+	with open("./config/bot.json", "r") as file:
+		is_su = json.load(file)["su"]
+	if str(update.message.chat_id) == is_su:
+		print("Queue stopped")
+		context.job_queue.stop()
--- todo-manager-bot.py
+++ todo-manager-bot.py
@@ -1,16 +1,19 @@
 from telegram.ext import Updater, CommandHandler
 from submodules import user_input as ui
+from submodules import send_notifications as sn
 import json
 
 def main():
-    print("Running...")
-    with open("./config/token.json", "r") as file:
-        token = json.load(file)["token"]
-    updater = Updater(token, use_context=True, workers=4)
-    dp = updater.dispatcher
-    dp.add_handler(CommandHandler("link", ui.link_user, pass_args=True))
-    updater.start_polling()
-    updater.idle()
+	print("Running...")
+	with open("./config/token.json", "r") as file:
+		token = json.load(file)["token"]
+	updater = Updater(token, use_context=True, workers=4)
+	dp = updater.dispatcher
+	dp.add_handler(CommandHandler("link", ui.link_user, pass_args=True))
+	dp.add_handler(CommandHandler("start", sn.callback_timer, pass_job_queue=True))
+	dp.add_handler(CommandHandler("stop", sn.stop_timer, pass_job_queue=True))
+	updater.start_polling()
+	updater.idle()
 
 if __name__ == '__main__':
-    main()
+	main()
