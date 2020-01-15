36c21b3a629a597b4514139b6e76832deadef489
tjtanjin <cjtanjin@gmail.com>
2020-01-16 02:15:39 +0800

    Fixed timing for reminders. Added entertainment options. Added code comments.

--- submodules/send_notifications.py
+++ submodules/send_notifications.py
@@ -2,6 +2,12 @@ from telegram import ParseMode
 import json, requests, datetime, pytz
 
 def check_user_tasks(context):
+	"""
+	Function to check the task of users and decide who should receive reminders for those expiring within the next 3 days. 
+	Reminders are sent once at 12am per day.
+	Args:
+        context: default telegram arg
+	"""
 	print("Checking to send reminders...")
 	with open("./config/endpoint.json", "r") as file:
 		endpoint = json.load(file)["endpoint"]
@@ -28,12 +34,26 @@ def check_user_tasks(context):
 			if len(expiring_tasks) != 0:
 				send_reminders(user["name"], user["telegram_id"], expiring_tasks, context)
 
-def getDifference(deadline, current_date): 
+def getDifference(deadline, current_date):
+	"""
+	Function to get the day difference between current date and due date.
+	Args:
+		deadline: the date to compare with today
+		current_date: the date today
+	"""
 	deadlineparts = deadline.split('-');
 	new_deadline = datetime.date(int(deadlineparts[0]), int(deadlineparts[1]), int(deadlineparts[2]))
 	return (new_deadline - current_date).days
 
 def send_reminders(todo_name, telegram_id, tasks, context):
+	"""
+	Function to send reminders out to users.
+	Args:
+		todo_name: username on todo manager account
+		telegram_id: telegram_id of the user
+		tasks: tasks that are expiring
+		context: default telegram arg
+	"""
 	string = "Hello " + todo_name + ", this is a daily reminder that your following tasks are expiring soon:\n\n" + "Task Name ----- Days Left\n"
 	for task in tasks:
 		task_name, days_left = formatMessage(task[0], str(task[1]))
@@ -42,6 +62,12 @@ def send_reminders(todo_name, telegram_id, tasks, context):
 	context.bot.send_message(chat_id=telegram_id, text=string, parse_mode=ParseMode.HTML)
 
 def formatMessage(task_name, days_left):
+	"""
+	Function to format the reminder message before it is sent to users.
+	Args:
+		task_name: name of the task
+		days_left: days left to task expiry
+	"""
 	for chars in range(0, 10):
 		if len(task_name) < 9:
 			task_name = task_name + " "
@@ -60,13 +86,25 @@ def formatMessage(task_name, days_left):
 	return [task_name, days_left]
 
 def callback_timer(update, context):
+	"""
+	Function to control the job queue for telegram (start).
+	Args:
+		update: default telegram arg
+		context: default telegram arg
+	"""
 	with open("./config/bot.json", "r") as file:
 		is_su = json.load(file)["su"]
 	if str(update.message.chat_id) == is_su:
 		print("Queue started")
-		context.job_queue.run_daily(check_user_tasks, datetime.time(17, 43, 00), context=context)
+		context.job_queue.run_daily(check_user_tasks, datetime.time(16, 00, 00), context=context)
 
 def stop_timer(update, context):
+	"""
+	Function to control the job queue for telegram (stop).
+	Args:
+		update: default telegram arg
+		context: default telegram arg
+	"""
 	with open("./config/bot.json", "r") as file:
 		is_su = json.load(file)["su"]
 	if str(update.message.chat_id) == is_su:
--- submodules/user_input.py
+++ submodules/user_input.py
@@ -1,7 +1,13 @@
-from telegram import ParseMode
+from telegram import ParseMode, InlineKeyboardButton, InlineKeyboardMarkup
 import json, requests
 
 def link_user(update, context):
+    """
+    Function to link user with their todo manager account.
+    Args:
+        update: default telegram arg
+        context: default telegram arg
+    """
     with open("./config/endpoint.json", "r") as file:
         endpoint = json.load(file)["endpoint"]
     res = requests.post(endpoint + "/link", data = {"email": context.args[0], "telegram_id": update.message.chat_id, "telegram_handle": update.message.from_user.username})
@@ -15,9 +21,40 @@ def link_user(update, context):
         update.message.reply_text("An error has occurred, please contact an administrator.")
     return None
 
+def entertain_me(update, context):
+    """
+    Function to show entertainment options to users.
+    Args:
+        update: default telegram arg
+        context: default telegram arg
+    """
+    reply_markup = InlineKeyboardMarkup(build_menu([InlineKeyboardButton("Play Age Of Empires", url="https://t.me/Ageofempire_bot")], n_cols=2))
+    update.message.reply_text("Why not show some support for my game? :)", reply_markup=reply_markup)
+
 def show_help(update, context):
+    """
+    Function to show help options to users.
+    Args:
+        update: default telegram arg
+        context: default telegram arg
+    """
     update.message.reply_text("""Here are the currently available commands:\n
         <b>/link (email)</b> - links your telegram and todo manager accounts\n
+        <b>/entertain</b> - shows a list of entertainment options\n
         <b>/help</b> - displays the available commands\n
 Have ideas and suggestions for this mini project? Head over to the <a href="https://github.com/tjtanjin/todo_website">Project Repository</a>!""", parse_mode=ParseMode.HTML, disable_web_page_preview=True)
     return None
+
+def build_menu(buttons, n_cols, header_buttons=None, footer_buttons=None):
+    """
+    Function to build the menu buttons to show users.
+    Args:
+        buttons: buttons to press
+        n_cols: number of cols
+    """
+    menu = [buttons[i:i + n_cols] for i in range(0, len(buttons), n_cols)]
+    if header_buttons:
+        menu.insert(0, header_buttons)
+    if footer_buttons:
+        menu.append(footer_buttons)
+    return menu
